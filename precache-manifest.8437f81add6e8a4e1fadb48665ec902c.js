67e89209ddac301630bbd67c351054f2bf97d152
Alexey Oblomov <alexey.oblomov@mail.ru>
2020-04-18 14:39:36 +0300

    Updates

---
+++ precache-manifest.8437f81add6e8a4e1fadb48665ec902c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28fcd116ff48a98e92b66eccba41e74",
+    "url": "/form_and_nodejs/index.html"
+  },
+  {
+    "revision": "596e85dc7c73fa736c3a",
+    "url": "/form_and_nodejs/static/css/main.695ab48d.chunk.css"
+  },
+  {
+    "revision": "e155554a249464edb5af",
+    "url": "/form_and_nodejs/static/js/2.8a430ca9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/form_and_nodejs/static/js/2.8a430ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596e85dc7c73fa736c3a",
+    "url": "/form_and_nodejs/static/js/main.e0657493.chunk.js"
+  },
+  {
+    "revision": "c8e2fc094ad8a67d1869",
+    "url": "/form_and_nodejs/static/js/runtime-main.caa97570.js"
+  }
+]);
